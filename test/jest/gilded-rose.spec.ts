c4661626ca1220023a7e2b716d4c052df95b9c1a
Krystian Kat <krystian.kat@protonmail.com>
2022-03-31 10:50:38 +0200

    test: add quality should decrease by 1 for day

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,19 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+describe("Gilded Rose", () => {
+  describe("item has proper name", () => {
+    it("should be fixme", () => {
+      const gildedRose = new GildedRose([new Item("fixme", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe("fixme");
+    });
+  });
+
+  describe("decrease quality for normal item each day", () => {
+    it("quality should decrease by 1 for day", () => {
+      const gildedRose = new GildedRose([new Item("example item", 1, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(9);
+    });
   });
 });
