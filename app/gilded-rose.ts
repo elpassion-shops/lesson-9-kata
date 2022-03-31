162cfd112e1e361d52f26ba1aa968c97c947e0c4
Michał Warda <michal.warda@elpassion.pl>
2022-03-31 12:41:41 +0200

    Extract updateItemQuality

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -21,37 +21,41 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') continue;
-      if (!this.SPECIAL_ITEMS.includes(this.items[i].name)) {
-        this.items[i].quality -= this.items[i].sellIn <= 0 ? 2 : 1;
-        if (this.items[i].quality < 0) this.items[i].quality = 0
-        this.items[i].sellIn -= 1;
-        continue
+      this.updateItemQuality(i)
+    }
+    return this.items;
+  }
+
+  private updateItemQuality(i: number) {
+    if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') return;
+    if (!this.SPECIAL_ITEMS.includes(this.items[i].name)) {
+      this.items[i].quality -= this.items[i].sellIn <= 0 ? 2 : 1;
+      if (this.items[i].quality < 0) this.items[i].quality = 0
+      this.items[i].sellIn -= 1;
+      return;
+    }
+    if (this.items[i].name === 'Aged Brie') {
+      this.items[i].quality += this.items[i].sellIn <= 0 ? 2 : 1;
+      if (this.items[i].quality > 50) this.items[i].quality = 50
+      this.items[i].sellIn -= 1;
+      return;
+    }
+    if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+      let qualityChange = 0;
+      if (this.items[i].sellIn > 10) {
+        qualityChange = 1;
+      } else if (this.items[i].sellIn > 5) {
+        qualityChange = 2;
+      } else if (this.items[i].sellIn > 0) {
+        qualityChange = 3;
+      } else {
+        this.items[i].quality = 0;
       }
-      if (this.items[i].name === 'Aged Brie') {
-        this.items[i].quality += this.items[i].sellIn <= 0 ? 2 : 1;
-        if (this.items[i].quality > 50) this.items[i].quality = 50
-        this.items[i].sellIn -= 1;
-        continue
-      }
-      if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
-        let qualityChange = 0;
-        if (this.items[i].sellIn > 10) {
-          qualityChange = 1;
-        } else if (this.items[i].sellIn > 5) {
-          qualityChange = 2;
-        } else if (this.items[i].sellIn > 0) {
-          qualityChange = 3;
-        } else {
-          this.items[i].quality = 0;
-        }
-        this.items[i].quality += qualityChange;
-        if (this.items[i].quality > 50) this.items[i].quality = 50;
-        this.items[i].sellIn -= 1;
-        continue
-      }
+      this.items[i].quality += qualityChange;
+      if (this.items[i].quality > 50) this.items[i].quality = 50;
+      this.items[i].sellIn -= 1;
+      return;
     }
 
-    return this.items;
   }
 }
