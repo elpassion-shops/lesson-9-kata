8bf5027f50bfef02d2cfadaac223d69e86854817
Krystian Kat <krystian.kat@protonmail.com>
2022-03-31 11:47:59 +0200

    test: refactor tests for normal items

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,27 +1,29 @@
 import { Item, GildedRose } from "@/gilded-rose";
 
 describe("Gilded Rose", () => {
-  describe("item has proper name", () => {
-    it("should be fixme", () => {
-      const gildedRose = new GildedRose([new Item("fixme", 0, 0)]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].name).toBe("fixme");
+  describe("tests for normal items", () => {
+    describe("item has proper name", () => {
+      it("should be fixme", () => {
+        const gildedRose = new GildedRose([new Item("fixme", 0, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).toBe("fixme");
+      });
     });
-  });
 
-  describe("decrease quality for normal item each day", () => {
-    it("quality should decrease by 1 for day", () => {
-      const gildedRose = new GildedRose([new Item("example item", 1, 10)]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(9);
+    describe("decrease quality for normal item each day", () => {
+      it("quality should decrease by 1 for day", () => {
+        const gildedRose = new GildedRose([new Item("example item", 1, 10)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toBe(9);
+      });
     });
-  });
 
-  describe("decrease quality for normal item cannot be under 0", () => {
-    it("quality should be equal 0", () => {
-      const gildedRose = new GildedRose([new Item("example item", 1, 0)]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(0);
+    describe("decrease quality for normal item cannot be under 0", () => {
+      it("quality should be equal 0", () => {
+        const gildedRose = new GildedRose([new Item("example item", 1, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toBe(0);
+      });
     });
   });
 
