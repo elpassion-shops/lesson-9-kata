ab35f07fa3df8420684559320897fcc6389021a1
ireshka <ireshka@users.noreply.github.com>
2022-03-31 11:53:01 +0200

    feat: I do not believe how it covers all cases?

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,47 +1,78 @@
 import { Item, GildedRose } from "@/gilded-rose";
 import { ItemsEnum } from "../../types/items.enum";
 describe("Gilded Rose", () => {
-  describe("for all object expect of Sulfuras", () => {
-    it("shouldn't have quality below 0", () => {
-      const gildedRose1 = new GildedRose([new Item(ItemsEnum.ORDINARY, 10, 1)]);
-      gildedRose1.updateQuality();
-      gildedRose1.updateQuality();
-      expect(gildedRose1.items[0].quality).toBe(0);
-
-      const gildedRose2 = new GildedRose([new Item(ItemsEnum.BRIE, 0, 0)]);
-      gildedRose2.updateQuality();
-      gildedRose2.updateQuality();
-      expect(gildedRose2.items[0].quality).toBe(4);
-
-      // const gildedRose3 = new GildedRose([new Item(ItemsEnum.CONJURY, 0, 20)]);
-      // gildedRose3.updateQuality();
-      // gildedRose3.updateQuality();
-      // expect(gildedRose3.items[0].quality).toBe(0);
-
-      const gildedRose4 = new GildedRose([new Item(ItemsEnum.BACKSTAGE, 0, 1)]);
-      gildedRose4.updateQuality();
-      gildedRose4.updateQuality();
-      expect(gildedRose4.items[0].quality).toBe(0);
-
-      const gildedRose5 = new GildedRose([new Item(ItemsEnum.CONJURY, 0, 1)]);
-      gildedRose5.updateQuality();
-      gildedRose5.updateQuality();
-      expect(gildedRose5.items[0].quality).toBe(0);
-
-      const gildedRose6 = new GildedRose([new Item(ItemsEnum.SULFURAS, 0, 80)]);
-      gildedRose6.updateQuality();
-      gildedRose6.updateQuality();
-      expect(gildedRose6.items[0].quality).toBe(80);
-    });
-  });
-
   describe("for ordinary object", () => {
-    it("should lower quality by one", () => {
+    it("should lower quality by one before sell day", () => {
       const gildedRose1 = new GildedRose([
         new Item(ItemsEnum.ORDINARY, 10, 30),
       ]);
       gildedRose1.updateQuality();
       expect(gildedRose1.items[0].quality).toBe(29);
+    });
+
+    it("should lower quality twice after sell day", () => {
+      const gildedRose1 = new GildedRose([new Item(ItemsEnum.ORDINARY, 0, 30)]);
+      gildedRose1.updateQuality();
+      expect(gildedRose1.items[0].quality).toBe(28);
+    });
+  });
+
+  describe("for aged brie", () => {
+    it("should not increase quality above 50", () => {
+      const gildedRose1 = new GildedRose([new Item(ItemsEnum.BRIE, 10, 50)]);
+      gildedRose1.updateQuality();
+      expect(gildedRose1.items[0].quality).toBe(50);
+    });
+  });
+
+  describe("for sulfurus", () => {
+    it("should not change quality - always 80", () => {
+      const gildedRose1 = new GildedRose([
+        new Item(ItemsEnum.SULFURAS, 10, 80),
+      ]);
+      gildedRose1.updateQuality();
+      expect(gildedRose1.items[0].quality).toBe(80);
+    });
+  });
+
+  describe("for backstage pass", () => {
+    it("should not increase quality above 0", () => {
+      const gildedRose1 = new GildedRose([
+        new Item(ItemsEnum.BACKSTAGE, 14, 50),
+      ]);
+      gildedRose1.updateQuality();
+      gildedRose1.updateQuality();
+      expect(gildedRose1.items[0].quality).toBe(50);
+      expect(gildedRose1.items[0].sellIn).toBe(12);
+    });
+    it("should increase quality twice between 10 and 6 day before sell", () => {
+      const gildedRose1 = new GildedRose([
+        new Item(ItemsEnum.BACKSTAGE, 9, 10),
+      ]);
+      gildedRose1.updateQuality();
+      gildedRose1.updateQuality();
+      expect(gildedRose1.items[0].quality).toBe(14);
+      expect(gildedRose1.items[0].sellIn).toBe(7);
+    });
+
+    it("should increase quality three times between 5 and 0 day before sell", () => {
+      const gildedRose1 = new GildedRose([
+        new Item(ItemsEnum.BACKSTAGE, 4, 10),
+      ]);
+      gildedRose1.updateQuality();
+      gildedRose1.updateQuality();
+      expect(gildedRose1.items[0].quality).toBe(16);
+      expect(gildedRose1.items[0].sellIn).toBe(2);
+    });
+
+    it("should drop quality to zero after sell day", () => {
+      const gildedRose1 = new GildedRose([
+        new Item(ItemsEnum.BACKSTAGE, 0, 10),
+      ]);
+      gildedRose1.updateQuality();
+      gildedRose1.updateQuality();
+      expect(gildedRose1.items[0].quality).toBe(0);
+      expect(gildedRose1.items[0].sellIn).toBe(-2);
     });
   });
 });
