d93d72bac7daab177d7e5e44f9bab2c953bdeb7b
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-03-31 14:57:30 +0200

    add class NormalItem

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -12,6 +12,19 @@ export class Item {
   }
 }
 
+class NormalItem {
+constructor(
+private sellIn: number,
+private quality: number
+){}
+
+tick() {
+    this.quality -= this.sellIn <= 0 ? 2 : 1;
+    if (this.quality < 0) this.quality = 0;
+    this.sellIn -= 1;
+}
+}
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -24,63 +37,59 @@ export class GildedRose {
     "Sulfuras, Hand of Ragnaros",
     "Backstage passes to a TAFKAL80ETC concert",
   ];
-  //negujemy scenariusze - zanim usuniemy statry kod, to piszemy nowy
+
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      this.updateItemQuality(i);
-    }
+   this.items.forEach((_, i) => {
+     let item = this.items[i]
+      this.updateItemQuality(item);
+    })
     return this.items;
   }
 
-  private updateItemQuality(i: number) {
-    this.handleSulfuras(i);
-    this.handleNormalItems(i);
-    this.handleAgedBrie(i);
-    this.handleBackstagePasses(i);
+  private updateItemQuality(item: Item) {
+    this.handleNormalItems(item);
+    this.handleAgedBrie(item);
+    this.handleBackstagePasses(item);
   }
 
-  private handleSulfuras(i: number){
-    if (this.items[i].name === "Sulfuras, Hand of Ragnaros") return; 
-  }
-
-  private handleNormalItems(i: number){
-    if (!this.SPECIAL_ITEMS.includes(this.items[i].name)) {
-      this.items[i].quality -= this.items[i].sellIn <= 0 ? 2 : 1;
-      if (this.items[i].quality < 0) this.items[i].quality = 0;
-      this.items[i].sellIn -= 1;
-      return;
-    }
-  }
-  private handleAgedBrie(i: number) {
-    if (this.items[i].name === "Aged Brie") {
-      this.items[i].quality += this.items[i].sellIn <= 0 ? 2 : 1;
-      if (this.items[i].quality > 50) this.items[i].quality = 50;
-      this.items[i].sellIn -= 1;
-      return;
+  private handleNormalItems(item: Item){
+    if (!this.SPECIAL_ITEMS.includes(item.name)) {
+      item.quality -= item.sellIn <= 0 ? 2 : 1;
+      if (item.quality < 0) item.quality = 0;
+      item.sellIn -= 1;
     }
   }
 
-  private handleBackstagePasses(i: number) {
-    if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
+  private handleAgedBrie(item: Item) {
+    if (item.name === "Aged Brie") {
+      item.quality += item.sellIn <= 0 ? 2 : 1;
+      if (item.quality > 50) item.quality = 50;
+      item.sellIn -= 1;
+    }
+  }
+
+  private handleBackstagePasses(item: Item) {
+    if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
       let qualityChange = 0;
-      if (this.items[i].sellIn > 10) {
+      if (item.sellIn > 10) {
         qualityChange = 1;
-      } else if (this.items[i].sellIn > 5) {
+      } else if (item.sellIn > 5) {
         qualityChange = 2;
-      } else if (this.items[i].sellIn > 0) {
+      } else if (item.sellIn > 0) {
         qualityChange = 3;
       } else {
-        this.items[i].quality = 0;
+        item.quality = 0;
       }
 
-      this.items[i].quality += qualityChange;
-      if (this.items[i].quality > 50) this.items[i].quality = 50;
-      return;
+      item.quality += qualityChange;
+      if (item.quality > 50) item.quality = 50;
     }
   }
 }
 
 
-//najpierw napisaliśmy testy
-//później returnowaliśmy w początkowych częściach kodu, żeby zobaczyć co nie przechodzi i implementowaliśmy kod dla poszczególnych przypadków
+//najpierw napisaliśmy testy dla całej logiki biznesowej 
+//pogrupowaliśmy testy w oddzielne grupy dla każdego produktu
+//później returnowaliśmy w początkowych częściach kodu, żeby zobaczyć co przechodzi, a conie i implementowaliśmy kod dla poszczególnych przypadków
+//negujemy scenariusze - zanim usuniemy statry kod, to piszemy nowy
 //później wyekstraktowaliśmy konkretne metody prywatne dla klasy Gilded Rose, żeby metoda updateItemQuality była czytelniejsza
