5029785810f5d070018bae390616b0aa6332dc07
tomek <tomaszpawelkowalski@gmail.com>
2022-03-31 10:51:02 +0200

    Feat:Tests decrease quality with edge case.

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,21 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+
+  it.skip('should  decrease quality', () => {
+    const gildedRose = new GildedRose([new Item('example item', 1, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].sellIn).toBe(0);
+
+  })
+
+  it.skip('should not decrease quality below zero', () => {
+    const gildedRose = new GildedRose([new Item('example item', 1, 0)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
+
+  })
+
+
+
 });
