f1f8ccef7263983069e7e6e762ab7d5339d215cc
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-31 10:50:34 +0200

    test: quality decrease rate for regular items

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,17 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+describe("Gilded Rose", () => {
+  describe("GR for regular items", () => {
+    it("should decrease the quality of regular items by 1 every day", () => {
+      const gildedRose = new GildedRose([new Item("Normal item", 1, 1)]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toBe(0);
+    });
+
+    it("should decrease the quality of regular items by x 2 every day after the sell date", () => {
+      const gildedRose = new GildedRose([new Item("Normal item", 0, 10)]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toBe(8);
+    });
   });
 });
