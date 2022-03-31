814a6898e1d32624a8b18d6dc548ea84355228ef
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-31 10:52:26 +0200

    test: minimum quality for all items

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,6 +1,14 @@
 import { Item, GildedRose } from "@/gilded-rose";
 
 describe("Gilded Rose", () => {
+  describe("GR for all items", () => {
+    it("should not allow the quality of any items to fall below zero", () => {
+      const gildedRose = new GildedRose([new Item("Item", 1, 0)]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toBe(0);
+    });
+  });
+
   describe("GR for regular items", () => {
     it("should decrease the quality of regular items by 1 every day", () => {
       const gildedRose = new GildedRose([new Item("Normal item", 1, 1)]);
