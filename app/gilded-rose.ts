6b7c5d5c5397b8b3cc80d967cbf5c4af551098c8
KlaudiaDabrowska <klaudia@DESKTOP-U5H299B.localdomain>
2022-03-31 11:49:32 +0200

    test: Add tests

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -5,8 +5,10 @@ export class Item {
 
   constructor(name, sellIn, quality) {
     this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
+    this.sellIn = sellIn; //data waznosci w dniach przedmiotu na dzień dzisiejszy
+    this.quality = quality; //jakość przedmiotu
+    //na koniec dnia zawsze się zmieniają i wartość i jakość
+    //obsłużenie itemu conjured (dwa razy szybciej quality)
   }
 }
 
@@ -19,7 +21,7 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1
@@ -29,7 +31,7 @@ export class GildedRose {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
+            if (this.items[i].sellIn < 11) { 
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
               }
--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,64 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it("should decrease sellIn", () => {
+    const gildedRose = new GildedRose([new Item('example item', 1, 10)])
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+    expect(items[0].sellIn).toBe(0)
+  })
+
+  it("should not decrease quality below 0", () => {
+    const gildedRose = new GildedRose([new Item('example item', 1, 0)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(0)
+  })
+
+  it("should not decrease and increase quality or sellIn for Sulfuras", () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 10, 10)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(10)
+    expect(items[0].quality).toBe(10)
+  })
+
+  it("should increase quality of Aged Brie when day passes", () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 10, 10)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(9)
+    expect(items[0].quality).toBe(11)
+  })
+
+  it("should quality degrades twice as fast when the sell by date has passed", () => {
+    const gildedRose = new GildedRose([new Item('example', 0, 10)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1)
+    expect(items[0].quality).toBe(8)
+  })
+
+  it("should not quality more than 50", () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 1, 50)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(0)
+    expect(items[0].quality).not.toBe(51)
+  })
+
+  it("should increases quality by 2 of backstage when are 10 days or less", () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(9)
+    expect(items[0].quality).toBe(12)
+  })
+
+  it("should increases quality by 3 of backstage when are 5 days or less", () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(4)
+    expect(items[0].quality).toBe(13)
+  })
+
+  it("should quality of backstage drops to 0 after the concert", () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1)
+    expect(items[0].quality).toBe(0)
+  })
 });
