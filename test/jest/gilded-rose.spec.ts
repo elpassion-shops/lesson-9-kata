5aac2f55978a2c973197393b58ea6b5eb336146f
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-31 10:43:21 +0200

    test: should decrese quality for normal item each tick

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,14 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+describe("Gilded Rose", () => {
+  // it('should foo', () => {
+  //   const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  //   const items = gildedRose.updateQuality();
+  //   expect(items[0].name).toBe('fixme');
+  // });
+  it("should decrese quality for normal item each tick", () => {
+    const gildedRose = new GildedRose([new Item("example item", 1, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].sellIn).toBe(0);
   });
 });
