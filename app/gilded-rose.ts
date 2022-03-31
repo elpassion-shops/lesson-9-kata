b59ee0e22388ae244cbf3d7aef4729c4949984da
Michał Warda <michal.warda@elpassion.pl>
2022-03-31 12:46:29 +0200

    Extract handleNormalItems

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -29,11 +29,7 @@ export class GildedRose {
   private updateItemQuality(i: number) {
     this.handleSulfuras(i);
     this.handleNormalItems(i);
-    if (this.items[i].name === 'Aged Brie') {
-      this.items[i].quality += this.items[i].sellIn <= 0 ? 2 : 1;
-      if (this.items[i].quality > 50) this.items[i].quality = 50
-      this.items[i].sellIn -= 1;
-    }
+    this.handleAgedBrie(i);
     if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
       let qualityChange = 0;
       if (this.items[i].sellIn > 10) {
@@ -47,6 +43,14 @@ export class GildedRose {
       }
       this.items[i].quality += qualityChange;
       if (this.items[i].quality > 50) this.items[i].quality = 50;
+      this.items[i].sellIn -= 1;
+    }
+  }
+
+  private handleAgedBrie(i: number) {
+    if (this.items[i].name === 'Aged Brie') {
+      this.items[i].quality += this.items[i].sellIn <= 0 ? 2 : 1;
+      if (this.items[i].quality > 50) this.items[i].quality = 50
       this.items[i].sellIn -= 1;
     }
   }
