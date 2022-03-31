6130ed94b83ccc67cc52e256a6e080af75f25fab
ireshka <ireshka@users.noreply.github.com>
2022-03-31 11:09:33 +0200

    add enum for items

---
+++ types/items.enum.ts
@@ -0,0 +1,7 @@
+export enum ItemsEnum {
+  BRIE = "Aged Brie",
+  BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert",
+  SULFURAS = "Sulfuras, Hand of Ragnaros",
+  CONJURY = "Conjured Mana Cake",
+  ORDINARY = "Ordinary piece of wood",
+}
