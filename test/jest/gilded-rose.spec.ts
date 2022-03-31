53f094330c931587448e05de7e7d3e1a7d5f88b9
tomek <tomaszpawelkowalski@gmail.com>
2022-03-31 10:57:33 +0200

    should decrease twice as fast after due date.

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -2,20 +2,25 @@ import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
 
-  it.skip('should  decrease quality', () => {
+  it('should  decrease quality', () => {
     const gildedRose = new GildedRose([new Item('example item', 1, 10)]);
     gildedRose.updateQuality();
     expect(gildedRose.items[0].sellIn).toBe(0);
 
-  })
+  });
 
-  it.skip('should not decrease quality below zero', () => {
+  it('should not decrease quality below zero', () => {
     const gildedRose = new GildedRose([new Item('example item', 1, 0)]);
     gildedRose.updateQuality();
     expect(gildedRose.items[0].quality).toBe(0);
 
-  })
+  });
 
+  it('should decrease twice as fast when sell in has passed', () => {
+    const gildedRose = new GildedRose([new Item('example item', -1, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(8);
 
+  });
 
 });
