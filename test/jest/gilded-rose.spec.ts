a963af4f0018f6932d12012d74ac142cf192e38b
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-31 10:53:24 +0200

    test: “Aged Brie” actually increases in Quality the older it gets

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,11 +1,6 @@
 import { Item, GildedRose } from "@/gilded-rose";
 
 describe("Gilded Rose", () => {
-  // it('should foo', () => {
-  //   const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[0].name).toBe('fixme');
-  // });
   it("should decrese quality for normal item each tick", () => {
     const gildedRose = new GildedRose([new Item("example item", 1, 10)]);
     gildedRose.updateQuality();
@@ -16,5 +11,12 @@ describe("Gilded Rose", () => {
     gildedRose.updateQuality();
     gildedRose.updateQuality();
     expect(gildedRose.items[0].quality).toBeGreaterThanOrEqual(0);
+  });
+  it("“Aged Brie” actually increases in Quality the older it gets", () => {
+    const gildedRose = new GildedRose([new Item("Aged Brie", 5, 1)]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(4);
   });
 });
