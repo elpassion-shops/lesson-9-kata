56dfc7df14ce67ab5c4a1264ef29ce9569875303
Krystian Kat <krystian.kat@protonmail.com>
2022-04-03 18:56:33 +0200

    feat: implement updateQuality for Backstage passes

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -25,6 +25,18 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
+        if (this.items[i].sellIn <= 0) {
+          this.items[i].quality = 0;
+        } else if (this.items[i].sellIn <= 5) {
+          this.items[i].quality += 3;
+        } else this.items[i].quality += this.items[i].sellIn <= 10 ? 2 : 1;
+
+        if (this.items[i].quality > 50) this.items[i].quality = 50;
+        this.items[i].sellIn -= 1;
+        return this.items;
+      }
+
       if (this.items[i].name === "Aged Brie") {
         this.items[i].quality += this.items[i].sellIn <= 0 ? 2 : 1;
         if (this.items[i].quality > 50) this.items[i].quality = 50;
