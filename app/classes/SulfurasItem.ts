0ec99f81166949be72997da49362fd400693a71c
Krystian Kat <krystian.kat@protonmail.com>
2022-04-03 23:09:24 +0200

    fix: add handle to SurfurasItem constructor

--- app/classes/SulfurasItem.ts
+++ app/classes/SulfurasItem.ts
@@ -1,6 +1,10 @@
 import { Item } from "./Item";
 
 export class SulfurasItem extends Item {
+  constructor(name: string, sellIn: number, quality: number) {
+    super(name, sellIn, quality);
+    return this.handle();
+  }
   handle() {
     this.quality = 80;
     return this;
